5e469a6def551f58d9e5e6430f52464b628aeda0
Matthew Migliore <mmigliore311@gmail.com>
2018-10-11 13:33:13 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,5 +7,13 @@ def array_with_two_elements
 end
 
 def first_element(array)
-  first_element[0]
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
 end
